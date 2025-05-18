ae494e0b2f025c49e3d0f5c637182da482f8eb4a
bibash777225 <bibashghimire000@gmail.com>
2025-05-18 08:58:37 +0545

    minor change

--- card_practice/src/App.jsx
+++ card_practice/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Card from "./Components/Card"
+import Home from "./Components/Home"
 import BhaktapurDurbar from "./Pages/BhaktapurDubar"
+import Lumbini from "./Pages/Lumbini"
 
 function App() {
   return(
     <>
        <BrowserRouter>
        <Routes>
-        <Route path="/" element={<Card />} />
+        <Route path="/" element={<Home />} />
         <Route path="/BhaktapurDurbar" element={<BhaktapurDurbar />}/>
+        <Route path="/Lumbini" element={<Lumbini />}/>
+        
         </Routes>
 
        </BrowserRouter>
---
+++ card_practice/src/Pages/Lumbini.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar"
+
+function Lumbini(){
+    return(
+        <>
+        <Navbar/>
+        <h1> this is lumbini page</h1>
+        </>
+    )
+}
+export default Lumbini
