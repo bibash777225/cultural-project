266e5bb4bdb10e0431ed8e21a71085faf8c90f8a
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-05-19 07:32:51 +0545

    Bug fixed

--- card_practice/src/App.jsx
+++ card_practice/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from "./Components/Home"
+import Home from "./Pages/Home"
 import BhaktapurDurbar from "./Pages/BhaktapurDubar"
 import Lumbini from "./Pages/Lumbini"
 
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/BhaktapurDurbar" element={<BhaktapurDurbar />}/>
         <Route path="/Lumbini" element={<Lumbini />}/>
-        
         </Routes>
 
        </BrowserRouter>
